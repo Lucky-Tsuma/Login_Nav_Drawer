684d7879de9402001ef61a6f878a307f96e60e47
Lucky-Tsuma <tsumahmumba@gmail.com>
2021-06-29 19:28:05 +0300

    Replaced findViewById with ViewBinding

--- app/src/main/java/com/lucky/login/MainActivity.kt
+++ app/src/main/java/com/lucky/login/MainActivity.kt
@@ -1,71 +1,79 @@
 package com.lucky.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
-import android.widget.Button
 import android.widget.Toast
-import com.google.android.material.textfield.TextInputLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.lucky.login.databinding.ActivityMainBinding
 import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textInputEmail: TextInputLayout
-    lateinit var textInputPassword: TextInputLayout
-    lateinit var confirmBtn: Button
+    private lateinit var binding: ActivityMainBinding
+    /*A property we are going to use to create an instance of the ViewBinding class*/
 
     private val PASSWORD_PATTERN =
-        Pattern.compile("^" +
-                "(?=.*[0-9])" +
-                "(?=.*[a-zA-Z])" +
-                "(?=\\S+$)" +
-                ".{6,}" +
-                "$")
+        Pattern.compile(
+            "^" +
+                    "(?=.*[0-9])" +
+                    "(?=.*[a-zA-Z])" +
+                    "(?=\\S+$)" +
+                    ".{6,}" +
+                    "$"
+        )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textInputEmail = findViewById(R.id.input_email)
-        textInputPassword = findViewById(R.id.input_password)
-        confirmBtn = findViewById(R.id.confirm_button)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        /*An instance of the ViewBinding class i.e the name of the xml file but PascalCased*/
+        val view = binding.root
+        setContentView(view)
+        /*Makes the root view of the xml file active, presents it to the screen*/
 
-        confirmBtn.setOnClickListener {
+        binding.confirmButton.setOnClickListener {
+            /*the names of the resources have been camelCased, confirmButton was confirm_button in
+            * the xml file*/
             if (!validateEmail() || !validatePassword()) {
                 return@setOnClickListener
             }
 
-            val email = textInputEmail.editText?.text.toString().trim()
-            val password = textInputPassword.editText?.text.toString().trim()
-            Toast.makeText(applicationContext, "Email: $email\nPassword: $password", Toast.LENGTH_SHORT).show()
+            val email = binding.inputEmail.editText?.text.toString().trim()
+            val password = binding.inputPassword.editText?.text.toString().trim()
+            Toast.makeText(
+                applicationContext,
+                "Email: $email\nPassword: $password",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
     private fun validateEmail(): Boolean {
-        val emailInput = textInputEmail.editText?.text.toString().trim()
+        val emailInput = binding.inputEmail.editText?.text.toString().trim()
 
-        return if(emailInput.isEmpty()) {
-            textInputEmail.error = "Failed, cannot be empty"
+        return if (emailInput.isEmpty()) {
+            binding.inputEmail.error = "Failed, cannot be empty"
             false
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()){
-            textInputEmail.error = "Please Enter a valid email address"
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            binding.inputEmail.error = "Please Enter a valid email address"
             false
         } else {
-            textInputEmail.error = null
+            binding.inputEmail.error = null
             true
         }
     }
 
     private fun validatePassword(): Boolean {
-        val passwordInput = textInputPassword.editText?.text.toString().trim()
+        val passwordInput = binding.inputPassword.editText?.text.toString().trim()
 
-        return if(passwordInput.isEmpty()) {
-            textInputPassword.error = "Failed, cannot be empty"
+        return if (passwordInput.isEmpty()) {
+            binding.inputPassword.error = "Failed, cannot be empty"
             false
-        } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()){
-            textInputPassword.error = "Weak Password"
+        } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
+            binding.inputPassword.error = "Weak Password"
             false
         } else {
-            textInputPassword.error = null
+            binding.inputPassword.error = null
             true
         }
     }
